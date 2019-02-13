7ba44f36f9fc0d4a0065294547d0ae8aeddac0dd
Enoka Jaona <Ejaona@gmail.com>
2019-02-13 17:06:47 -0600

    created endpoints for getPostById and create post

--- postRouter.js
+++ postRouter.js
@@ -12,4 +12,33 @@ postRouter.get("/", async (req, res) => {
   }
 });
 
+postRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await postDb.getById(id);
+    res.status(200).json({ success: true, post });
+  } catch ({ code, message }) {
+    res.status(code).json({ success: false, message: message });
+  }
+});
+
+// postRouter.get("/:id/posts", async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const post = await postDb.getUserPosts(id);
+//     res.status(200).json({ success: true, post });
+//   } catch ({ code, message }) {
+//     res.status(code).json({ success: false, message: message });
+//   }
+// });
+
+postRouter.post("/", async (req, res) => {
+  const newPost = req.body;
+  try {
+    const post = await postDb.insert(newPost);
+    res.status(201).json({ success: true, post });
+  } catch ({ code, message }) {
+    res.status(code).json({ success: false, message: message });
+  }
+});
 module.exports = postRouter;
